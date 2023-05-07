abfe979963645601224ba47e335f62bdb5031439
Matheus Cardoso de Oliveira <97033378+cardosoliveira@users.noreply.github.com>
2023-05-07 14:09:19 -0300

    commit #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http"
 
+import { CoreModule } from "./core/core.module"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    CoreModule,
     AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
