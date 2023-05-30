0ad65d0b49cee38ee59cacda305607c1f913d96b
Matheus Cardoso de Oliveira <97033378+cardosoliveira@users.noreply.github.com>
2023-05-29 23:21:05 -0300

    Login #2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: 'entries', loadChildren: "./pages/entries/entries.module#EntriesModule"},
   { path: 'categories', loadChildren: './pages/categories/shared/categories.module#CategoriesModule'},
   { path: 'reports', loadChildren: "./pages/reports/reports.module#ReportsModule"},
-  { path: 'login', loadChildren: "./pages/login/login.module#LoginModule"}
+  { path: '', loadChildren: "./pages/login/login.module#LoginModule"}
 ];
 
 @NgModule({
